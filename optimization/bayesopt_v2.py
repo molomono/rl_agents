859ab547959fa0388e9844f9a576151c6f03fe89
Brian Armstrong <BAR@demcon.local>
2019-07-04 15:49:17 +0200

    added a constants decleration file

--- optimization/bayesopt_v2.py
+++ optimization/bayesopt_v2.py
@@ -43,57 +43,62 @@ IMPORTANT:
 		
 	#i should also implement a Toy equation just to make sure GPyOpt is doing what it should be doing
 '''
-home_path = os.path.expanduser('~')
+# home_path = os.path.expanduser('~')
 
-agent = 'ppo'
+# agent = 'ppo'
 
-#Append new agents to these dictionaries:
-agent_preset = {'ddpg': 'ddpg_vrep_opt.py',
-				'ppo': 'ppo_vrep_opt.py'}
+# #Append new agents to these dictionaries:
+# agent_preset = {'ddpg': 'ddpg_vrep_opt.py',
+# 				'ppo': 'ppo_vrep_opt.py'}
 				
-agent_opt_dir = {'ddpg': 'ddpg_vrep_0',
-				 'ppo': 'ppo_vrep_opt_0'}
+# agent_opt_dir = {'ddpg': 'ddpg_vrep_0',
+# 				 'ppo': 'ppo_vrep_opt_0'}
 
 
 
-#TODO: Modify the bounds define the bounding box for the hyperparameters
-boundaries ={'example':
-                [{'name': 'e_d',    'type': 'continuous', 'domain': (0.9,0.9999)},
-                {'name': 'e_m',     'type': 'continuous', 'domain': (0.0,0.05)},
-                {'name': 'gamma',   'type': 'continuous', 'domain': (0.8,1.0)},
-                {'name': 'lr',      'type': 'continuous', 'domain': (0.0,0.05)},
-                {'name': 'lr_d',    'type': 'continuous', 'domain': (0.0,0.05)},
-                {'name': 'b_m',     'type': 'discrete',   'domain': (16, 32, 64, 128, 256)},
-                {'name': 'layer1',  'type': 'discrete',   'domain': (20,24,28,32,36,40)},
-                {'name': 'layer2',  'type': 'discrete',   'domain': (20,24,28,32,36,40)},
-                {'name': 'layer1_a','type': 'categorical','domain': (0,1,2)}, #['tanh','relu','linear']
-                {'name': 'layer2_a','type': 'categorical','domain': (0,1,2)}], 
-            'ddpg':
-                [{'name': 'actor_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
-                {'name': 'actor_layer_2_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
-                {'name': 'actor_layer_3_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
-                {'name': 'critic_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
-                {'name': 'critic_layer_2_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
-                {'name': 'critic_layer_3_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
-                {'name': 'discount_factor',         'type': 'continuous', 'domain': (0.9,1.0)}, 
-                {'name': 'actor_learning_rate',     'type': 'continuous', 'domain': (0.0001, 0.5)}, 
-                {'name': 'critic_learning_rate',    'type': 'continuous', 'domain': (0.0001, 0.5)}, 
-                {'name': 'exploration_factor',      'type': 'continuous', 'domain': (0.01,3.0)},
-                {'name': 'polyak',      			'type': 'continuous', 'domain': (0.0001,0.5)}],
-            'ppo':
-				[{'name': 'actor_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
-                {'name': 'actor_layer_2_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
-                {'name': 'actor_layer_3_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
-                {'name': 'critic_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
-                {'name': 'critic_layer_2_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
-                {'name': 'critic_layer_3_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
-                {'name': 'discount_factor',         'type': 'continuous', 'domain': (0.9,1.0)}, 
-                {'name': 'actor_learning_rate',     'type': 'continuous', 'domain': (0.0001, 0.05)}, 
-                {'name': 'critic_learning_rate',    'type': 'continuous', 'domain': (0.0001, 0.05)}, 
-                {'name': 'gae_lambda',    	  		'type': 'continuous', 'domain': (0.8, 0.999)},
-                {'name': 'beta_entropy', 			'type': 'continuous', 'domain': (0.0001,0.05)},
-                {'name': 'clip_likelihood_ratio', 	'type': 'continuous', 'domain': (0.01, 1.0)},],
-            }
+# #TODO: Modify the bounds define the bounding box for the hyperparameters
+# boundaries ={'example':
+#                 [{'name': 'e_d',    'type': 'continuous', 'domain': (0.9,0.9999)},
+#                 {'name': 'e_m',     'type': 'continuous', 'domain': (0.0,0.05)},
+#                 {'name': 'gamma',   'type': 'continuous', 'domain': (0.8,1.0)},
+#                 {'name': 'lr',      'type': 'continuous', 'domain': (0.0,0.05)},
+#                 {'name': 'lr_d',    'type': 'continuous', 'domain': (0.0,0.05)},
+#                 {'name': 'b_m',     'type': 'discrete',   'domain': (16, 32, 64, 128, 256)},
+#                 {'name': 'layer1',  'type': 'discrete',   'domain': (20,24,28,32,36,40)},
+#                 {'name': 'layer2',  'type': 'discrete',   'domain': (20,24,28,32,36,40)},
+#                 {'name': 'layer1_a','type': 'categorical','domain': (0,1,2)}, #['tanh','relu','linear']
+#                 {'name': 'layer2_a','type': 'categorical','domain': (0,1,2)}], 
+#             'ddpg':
+#                 [{'name': 'actor_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+#                 {'name': 'actor_layer_2_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+#                 {'name': 'actor_layer_3_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+#                 {'name': 'critic_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+#                 {'name': 'critic_layer_2_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+#                 {'name': 'critic_layer_3_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+#                 {'name': 'discount_factor',         'type': 'continuous', 'domain': (0.9,1.0)}, 
+#                 {'name': 'actor_learning_rate',     'type': 'continuous', 'domain': (0.0001, 0.5)}, 
+#                 {'name': 'critic_learning_rate',    'type': 'continuous', 'domain': (0.0001, 0.5)}, 
+#                 {'name': 'exploration_factor',      'type': 'continuous', 'domain': (0.01,3.0)},
+#                 {'name': 'polyak',      			'type': 'continuous', 'domain': (0.0001,0.5)}],
+#             'ppo':
+# 				[{'name': 'actor_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+#                 {'name': 'actor_layer_2_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+#                 {'name': 'actor_layer_3_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+#                 {'name': 'critic_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+#                 {'name': 'critic_layer_2_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+#                 {'name': 'critic_layer_3_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+#                 {'name': 'discount_factor',         'type': 'continuous', 'domain': (0.9,1.0)}, 
+#                 {'name': 'actor_learning_rate',     'type': 'continuous', 'domain': (0.0001, 0.05)}, 
+#                 {'name': 'critic_learning_rate',    'type': 'continuous', 'domain': (0.0001, 0.05)}, 
+#                 {'name': 'gae_lambda',    	  		'type': 'continuous', 'domain': (0.8, 0.999)},
+#                 {'name': 'beta_entropy', 			'type': 'continuous', 'domain': (0.0001,0.05)},
+#                 {'name': 'clip_likelihood_ratio', 	'type': 'continuous', 'domain': (0.01, 1.0)},],
+#             }
+
+import sys
+from constants_and_spaces import * 
+agent = None # TODO: change this value to an sys.argv statement so you can pass the desired AI to tune
+
 
 #Retrieve the names of all the parameter variables being tuned:
 param_names = []
@@ -228,6 +233,10 @@ if __name__=="__main__":
     ''' Main function instantiates a gaussian process optimizer from the GPyOpt package and performs Bayesian Optimization
     within the search domain defined in the boundaries dict.
     '''
+
+    global agent
+    
+    agent = sys.argv[1]
     #Define the number of optimization iterations to run.
     initial_datapoints = 5
     max_iter = 25
---
+++ optimization/constants_and_spaces.py
@@ -0,0 +1,51 @@
+
+import os 
+
+home_path = os.path.expanduser('~')
+
+#Append new agents to these dictionaries:
+agent_preset = {'ddpg': 'ddpg_vrep_opt.py',
+				'ppo': 'ppo_vrep_opt.py'}
+
+#Decleration of the directory name used for each respective algorithm optimization cycle.				
+agent_opt_dir = {'ddpg': 'ddpg_vrep_0',
+				 'ppo': 'ppo_vrep_opt_0'}
+
+#Boundaries is a dict of each algorithms hyperparameter spaces.
+boundaries ={'example':
+                [{'name': 'e_d',    'type': 'continuous', 'domain': (0.9,0.9999)},
+                {'name': 'e_m',     'type': 'continuous', 'domain': (0.0,0.05)},
+                {'name': 'gamma',   'type': 'continuous', 'domain': (0.8,1.0)},
+                {'name': 'lr',      'type': 'continuous', 'domain': (0.0,0.05)},
+                {'name': 'lr_d',    'type': 'continuous', 'domain': (0.0,0.05)},
+                {'name': 'b_m',     'type': 'discrete',   'domain': (16, 32, 64, 128, 256)},
+                {'name': 'layer1',  'type': 'discrete',   'domain': (20,24,28,32,36,40)},
+                {'name': 'layer2',  'type': 'discrete',   'domain': (20,24,28,32,36,40)},
+                {'name': 'layer1_a','type': 'categorical','domain': (0,1,2)}, #['tanh','relu','linear']
+                {'name': 'layer2_a','type': 'categorical','domain': (0,1,2)}], 
+            'ddpg':
+                [{'name': 'actor_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+                {'name': 'actor_layer_2_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+                {'name': 'actor_layer_3_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+                {'name': 'critic_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+                {'name': 'critic_layer_2_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+                {'name': 'critic_layer_3_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+                {'name': 'discount_factor',         'type': 'continuous', 'domain': (0.9,1.0)}, 
+                {'name': 'actor_learning_rate',     'type': 'continuous', 'domain': (0.0001, 0.5)}, 
+                {'name': 'critic_learning_rate',    'type': 'continuous', 'domain': (0.0001, 0.5)}, 
+                {'name': 'exploration_factor',      'type': 'continuous', 'domain': (0.01,3.0)},
+                {'name': 'polyak',      			'type': 'continuous', 'domain': (0.0001,0.5)}],
+            'ppo':
+				[{'name': 'actor_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+                {'name': 'actor_layer_2_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+                {'name': 'actor_layer_3_nodes',     'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+                {'name': 'critic_layer_1_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+                {'name': 'critic_layer_2_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3)}, 
+                {'name': 'critic_layer_3_nodes',    'type': 'categorical',   'domain': (0, 1, 2, 3, 4)}, 
+                {'name': 'discount_factor',         'type': 'continuous', 'domain': (0.9,1.0)}, 
+                {'name': 'actor_learning_rate',     'type': 'continuous', 'domain': (0.0001, 0.05)}, 
+                {'name': 'critic_learning_rate',    'type': 'continuous', 'domain': (0.0001, 0.05)}, 
+                {'name': 'gae_lambda',    	  		'type': 'continuous', 'domain': (0.8, 0.999)},
+                {'name': 'beta_entropy', 			'type': 'continuous', 'domain': (0.0001,0.05)},
+                {'name': 'clip_likelihood_ratio', 	'type': 'continuous', 'domain': (0.01, 1.0)},],
+            }
